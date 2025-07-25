a31d7202987ab5ea7c38344e1880d9134a29f6d6
simran <simranchand@gmail.com>
2025-07-26 02:20:50 +0530

    Initial commit

---
+++ practise_JavaScript.js
@@ -0,0 +1,35 @@
+console.log("Hello World! This is Simran. THis is my first code in JavaScript.")
+console.log("---------------------------------");
+var a = 5;
+var b = 6;
+var c = a+b;
+console.log(c);  //Used to print.
+const pi = 3.14;
+//Let keywords = Initializing the variable
+{
+    let d = 10;
+    console.log(d);
+}
+console.log(pi);
+
+
+console.log("-------------------------------");
+//Example
+let name = "Simran";
+constage = 23;
+var isStudent = true;
+let numbers = [1, 2, 3, 4];
+let person = {name: "Simran, age:21"};
+
+console.log(name);
+console.log(isStudent);
+console.log(person);
+console.log(numbers[1]);
+
+
+console.log("------------------------------------------");
+function fn()
+{
+    console.log("Function executer");
+}
+fn();
